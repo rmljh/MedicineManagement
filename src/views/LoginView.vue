6f242c913afe329e441cda1beb1c9f6dfce2100b
ljh <rmljh@qq.com>
2023-05-31 16:19:21 +0800

    123

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -7,7 +7,7 @@
         <p class="loginTitle">药品全过程防伪溯源区块链应用管理系统</p>
         <div class="loginForm">
           <el-input class="logininput" placeholder="用户名"     :prefix-icon="userIcon" v-model="pageInfo.username"/>
-          <el-input class="logininput" placeholder="密&#8195码" :prefix-icon="pswdIcon" v-model="pageInfo.password"/>
+          <el-input class="logininput" placeholder="密&#8195码" :prefix-icon="pswdIcon" v-model="pageInfo.password" type="password"/>
         </div>
         <el-row><el-button  class="button1" type="primary" @click="loginHandle" style="display:block;margin:0 auto">登录</el-button></el-row>
         <el-row><el-button  class="button2" type="primary" @click="pageInfo.signInFlag=true" style="display:block;margin:0 auto">注册</el-button></el-row>
@@ -26,7 +26,7 @@
                 <el-option v-for="item in pageInfo.roleOption" :key="item" :value="item"></el-option>
               </el-select>
               <el-input class="registerinput" placeholder="用&#8194户&#8194名"      :prefix-icon="userIcon" v-model="pageInfo.registerUsername"/>
-              <el-input class="registerinput" placeholder="密&#8195&#8195码"  :prefix-icon="pswdIcon" v-model="pageInfo.registerPassword"/>
+              <el-input class="registerinput" placeholder="密&#8195&#8195码"  :prefix-icon="pswdIcon" v-model="pageInfo.registerPassword" type="password"/>
               <el-input class="registerinput" placeholder="厂商代码"    :prefix-icon="creditIcon" v-model="pageInfo.registerCreditNumber"/>
           <!-- </div> -->
           <el-row><el-button class="button3" type="primary" @click="signInHandle" >确认</el-button></el-row>
